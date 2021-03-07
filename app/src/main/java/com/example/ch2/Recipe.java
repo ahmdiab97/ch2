54c234f92c5ba0cb3c93893cabc8da41c6b14659
ahmdiab97 <79974376+ahmdiab97@users.noreply.github.com>
2021-03-07 18:04:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ch2/Recipe.java
@@ -0,0 +1,15 @@
+package com.example.ch2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
